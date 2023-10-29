6397663739bf574dbd85ecc2dd9927f87b89c093
MohaJabri <mohammed.jabri.developer@gmail.com>
2023-10-28 20:40:02 -0500

    agregando redux-action de servicios al fronted

---
+++ Frontend/src/redux/actions/services.jsx
@@ -0,0 +1,247 @@
+import axios from "axios";
+import {
+  GET_SERVICES_SUCCESS,
+  GET_SERVICES_FAIL,
+  GET_SERVICES_BY_ARRIVAL_SUCCESS,
+  GET_SERVICES_BY_ARRIVAL_FAIL,
+  GET_SERVICES_BY_SOLD_SUCCESS,
+  GET_SERVICES_BY_SOLD_FAIL,
+  GET_SERVICE_SUCCESS,
+  GET_SERVICE_FAIL,
+  SEARCH_SERVICES_SUCCESS,
+  SEARCH_SERVICES_FAIL,
+  RELATED_SERVICES_SUCCESS,
+  RELATED_SERVICES_FAIL,
+  FILTER_SERVICES_SUCCESS,
+  FILTER_SERVICES_FAIL,
+} from "./types";
+
+const backend_url = import.meta.env.VITE_API_URL;
+
+export const get_services = () => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      `${backend_url}/api/service/get-services`,
+      config
+    );
+
+    if (res.status === 200) {
+      dispatch({
+        type: GET_SERVICES_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: GET_SERVICES_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_SERVICES_FAIL,
+    });
+  }
+};
+
+export const get_services_by_arrival = () => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      `${backend_url}/api/service/get-servives?sortBy=date_created&order=desc&limit=3`,
+      config
+    );
+
+    if (res.status === 200) {
+      dispatch({
+        type: GET_SERVICES_BY_ARRIVAL_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: GET_SERVICES_BY_ARRIVAL_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_SERVICES_BY_ARRIVAL_FAIL,
+    });
+  }
+};
+
+export const get_services_by_sold = () => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      `${backend_url}/api/service/get-services?sortBy=sold&order=desc&limit=3`,
+      config
+    );
+
+    if (res.status === 200) {
+      dispatch({
+        type: GET_SERVICES_BY_SOLD_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: GET_SERVICES_BY_SOLD_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_SERVICES_BY_SOLD_FAIL,
+    });
+  }
+};
+
+export const get_service = (serviceId) => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      `${backend_url}/api/service/get-service/${serviceId}`,
+      config
+    );
+
+    if (res.status === 200) {
+      dispatch({
+        type: GET_SERVICE_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: GET_SERVICE_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: GET_SERVICES_FAIL,
+    });
+  }
+};
+
+export const get_related_services = (serviceId) => async (dispatch) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      `${backend_url}/api/service/related/${serviceId}`,
+      config
+    );
+
+    if (res.status === 200 && !res.data.error) {
+      dispatch({
+        type: RELATED_SERVICES_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: RELATED_SERVICES_FAIL,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: RELATED_SERVICES_FAIL,
+    });
+  }
+};
+
+export const get_filtered_services =
+  (category_id, price_range, sort_by, order) => async (dispatch) => {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({
+      category_id,
+      price_range,
+      sort_by,
+      order,
+    });
+
+    try {
+      const res = await axios.post(
+        `${backend_url}/api/service/by/search`,
+        body,
+        config
+      );
+
+      if (res.status === 200 && !res.data.error) {
+        dispatch({
+          type: FILTER_SERVICES_SUCCESS,
+          payload: res.data,
+        });
+      } else {
+        dispatch({
+          type: FILTER_SERVICES_FAIL,
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: FILTER_SERVICES_FAIL,
+      });
+    }
+  };
+
+export const get_search_SERVICES =
+  (search, category_id) => async (dispatch) => {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({
+      search,
+      category_id,
+    });
+
+    try {
+      const res = await axios.post(
+        `${backend_url}/api/service/search`,
+        body,
+        config
+      );
+
+      if (res.status === 200) {
+        dispatch({
+          type: SEARCH_SERVICES_SUCCESS,
+          payload: res.data,
+        });
+      } else {
+        dispatch({
+          type: SEARCH_SERVICES_FAIL,
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: SEARCH_SERVICES_FAIL,
+      });
+    }
+  };
