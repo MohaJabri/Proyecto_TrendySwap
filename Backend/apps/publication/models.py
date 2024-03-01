19036abacd81ca49589f7da4c94ffc9635bb9905
MohaJabri <mohammed.jabri.developer@gmail.com>
2024-03-01 03:32:31 +0000

    modificando modelo de publicación

--- Backend/apps/publication/models.py
+++ Backend/apps/publication/models.py
@@ -6,21 +6,23 @@ User=settings.AUTH_USER_MODEL
 # Create your models here.
 
 class Publication(models.Model):
-    service_requested=models.CharField(max_length=100)
+    service_requested = models.CharField(max_length=100)
     object_offered = models.TextField()
-    location=models.CharField(max_length=200)
-    description=models.TextField()
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
-    photo=models.ImageField(upload_to='photos/%Y/%m/',blank=True, null=True)
-    date_created=models.DateTimeField(default=datetime.now, blank=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.service_requested
-    
+
     def get_thumbnail(self):
-        print(self.photo)
-        if self.photo:
-            return self.photo.url
+        if self.publicationimage_set.exists():
+            return self.publicationimage_set.first().image.url
         else:
             return 'https://via.placeholder.com/300x300.png?text=No+Image'
-        
+
+class PublicationImage(models.Model):
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/%Y/%m/', blank=True, null=True)
