662e77835441d8295212fad7a049fe09aa36dcf3
MohaJabri <mohammed.jabri.developer@gmail.com>
2024-05-27 09:51:23 -0500

    agregando pagina about

--- Frontend/src/containers/pages/About.jsx
+++ Frontend/src/containers/pages/About.jsx
@@ -1,12 +1,16 @@
+import Layout from "../../hocs/Layout";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        This is a simple project to practice the MERN stack. It is a platform
-        where users can exchange books.
-      </p>
-    </div>
+    <Layout>
+      <div className="bg-white min-h-screen">
+        <h1>About</h1>
+        <p>
+          This is a simple project to practice the MERN stack. It is a platform
+          where users can exchange books.
+        </p>
+      </div>
+    </Layout>
   );
 };
 
