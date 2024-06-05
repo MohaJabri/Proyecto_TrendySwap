92674363cc1b486cb33d1e82473ac73a96e0d53c
MohaJabri <mohammed.jabri.developer@gmail.com>
2024-06-05 17:42:53 -0500

    agregando miniatura en publicacion y usuaro en el panel admin

--- Backend/apps/user/serializers.py
+++ Backend/apps/user/serializers.py
@@ -2,9 +2,15 @@ from django.contrib.auth import get_user_model
 User=get_user_model()
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer
+from apps.user_profile.models import UserProfile
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_image']
 
 class UserCreateSerializer(UserCreateSerializer):
+    profile = UserProfileSerializer(source='userprofile', read_only=True)
     class Meta(UserCreateSerializer.Meta):
         model=User
-        fields=('id','email', 'first_name', 'last_name', 'get_full_name' , 'get_short_name', 'is_staff')
+        fields=('id','email', 'first_name', 'last_name', 'get_full_name' , 'get_short_name', 'is_staff', 'profile')
--- Frontend/src/containers/pages/UserPublications.jsx
+++ Frontend/src/containers/pages/UserPublications.jsx
@@ -13,6 +13,8 @@ import { connect } from "react-redux";
 import Pagination from "../../components/navigation/Pagination";
 import Layout from "../../hocs/Layout";
 import { useEffect } from "react";
+const backend_url = import.meta.env.VITE_API_URL;
+
 const UserPublications = ({
   user,
   delete_publication,
@@ -72,14 +74,46 @@ const UserPublications = ({
   return (
     <Layout>
       <div className="overflow-x-auto min-h-screen">
+        <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+          <div className="w-full md:w-1/2">
+            <form className="flex items-center">
+              <label htmlFor="simple-search" className="sr-only">
+                Search
+              </label>
+              <div className="relative w-full">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <input
+                  //value={search}
+                  //onChange={(e) => handleSearch(e.target.value)}
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 "
+                  placeholder="Search"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
         <table className="w-full text-sm text-left text-gray-500 ">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
             <tr>
               <th scope="col" className="px-4 py-3">
-                publication ID
+                Foto
               </th>
               <th scope="col" className="px-4 py-3">
-                Name
+                Servicio solicitado
               </th>
 
               <th scope="col" className="px-4 py-3">
@@ -111,7 +145,31 @@ const UserPublications = ({
                       scope="row"
                       className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap "
                     >
-                      {publication.id}
+                      <span className="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100">
+                        {publication.photos[0]?.image ? (
+                          <img
+                            src={`${backend_url}${publication.photos[0]?.image}`}
+                            alt="publication"
+                          />
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="800px"
+                            height="800px"
+                            viewBox="0 0 120 120"
+                            fill="none"
+                            className="w-full h-full"
+                          >
+                            <rect width="120" height="120" fill="#EFF1F3" />
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M33.2503 38.4816C33.2603 37.0472 34.4199 35.8864 35.8543 35.875H83.1463C84.5848 35.875 85.7503 37.0431 85.7503 38.4816V80.5184C85.7403 81.9528 84.5807 83.1136 83.1463 83.125H35.8543C34.4158 83.1236 33.2503 81.957 33.2503 80.5184V38.4816ZM80.5006 41.1251H38.5006V77.8751L62.8921 53.4783C63.9172 52.4536 65.5788 52.4536 66.6039 53.4783L80.5006 67.4013V41.1251ZM43.75 51.6249C43.75 54.5244 46.1005 56.8749 49 56.8749C51.8995 56.8749 54.25 54.5244 54.25 51.6249C54.25 48.7254 51.8995 46.3749 49 46.3749C46.1005 46.3749 43.75 48.7254 43.75 51.6249Z"
+                              fill="#687787"
+                            />
+                          </svg>
+                        )}
+                      </span>
                     </th>
 
                     <td className="px-4 py-3">
