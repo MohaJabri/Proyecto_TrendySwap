64da08a100792d0bf872ca49214e56e1aefa68c7
Jabri\mohjabri <mohammed.jabri.developer@gmail.com>
2024-06-16 22:08:28 +0200

    corrección manage.py

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'NAME': config("DB_NAME"),
         'USER': config("DB_USER"),
         'PASSWORD': config("DB_PASSWORD"),
-        'HOST': 'database',
+        'HOST': config("DB_HOST"),
         'PORT': config("DB_PORT"),
 
     }
