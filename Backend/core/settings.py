970e14d8bec21be451ed453d8c4111fe6cfe6343
Jabri\mohjabri <mohammed.jabri.developer@gmail.com>
2024-06-22 22:02:17 +0200

    Change http to https

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -226,8 +226,8 @@ AUTH_USER_MODEL = 'user.UserAccount'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
