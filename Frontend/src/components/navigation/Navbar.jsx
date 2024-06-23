8f7e1177d82ed2f35f46caa44439853e53cdba94
MohaJabri <mohammed.jabri.developer@gmail.com>
2024-06-23 18:23:56 -0500

    correciones visuales al navbar

--- Frontend/src/components/navigation/Navbar.jsx
+++ Frontend/src/components/navigation/Navbar.jsx
@@ -300,7 +300,6 @@ function Navbar({
                       alt="Workflow"
                     />
                   </div>
-                  <div className="-mr-2">
                   <SearchBox
                   order={order}
                   search={search}
@@ -311,6 +310,8 @@ function Navbar({
                   onSubmit={onSubmit}
                   categories={categories}
                 />
+                  <div className="-mr-2">
+                  
                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500">
                       <span className="sr-only">Close menu</span>
                       <XIcon className="h-6 w-6" aria-hidden="true" />
