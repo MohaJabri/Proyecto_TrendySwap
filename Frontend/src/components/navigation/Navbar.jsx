a02e18f8df0e49cbabe9c0837aa96e6b3c75a3c2
MohaJabri <mohammed.jabri.developer@gmail.com>
2024-06-23 19:48:11 -0500

    correcion a la paginacio

--- Frontend/src/components/navigation/Navbar.jsx
+++ Frontend/src/components/navigation/Navbar.jsx
@@ -323,7 +323,9 @@ function Navbar({
               <div className="py-6 px-5">
                 <div className="grid grid-cols-2 gap-4">
                   <Link
-                    onClick={() => localStorage.setItem("currentPage", page)}
+                    onClick={() => {
+                      localStorage.setItem("currentPage", 1);
+                    }}
                     to="/swap"
                     className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
                   >
